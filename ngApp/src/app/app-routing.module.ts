f7b161476bd83733f985f89d5fa86faf81c6487e
Akshit Dhar <akshitdhar@live.com>
2018-07-13 22:53:47 +0530

    included bootstrap

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"expenses", component:ExpensesComponent},
+  { path:"login", component:LoginComponent},
+  { path:'register', component:RegisterComponent},
+  { path:'', redirectTo:"/login", pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
