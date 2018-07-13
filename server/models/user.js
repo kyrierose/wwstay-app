1aea145a423de8a29a5947100c5e374b6a7e08a5
Akshit Dhar <akshitdhar@live.com>
2018-07-13 22:27:22 +0530

    defined expense schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,24 @@ import mongoose from 'mongoose';
 //ES6 destructuring
 const {Schema} = mongoose;
 
+//Define image schema
+const imageSchema = new Schema({
+    name: String,
+    url: String
+});
+
+//Define expense schema
+const expenseSchema = new Schema({
+    expense_name: String,
+    price: Number,
+    timestamp: Date,
+    image: imageSchema
+});
+
 const userSchema = new Schema({
     email: String,
-    password: String
+    password: String,
+    expenses : [expenseSchema]
 });
 
 //third parameter specifies the collection name 
--- server/routes/api.js
+++ server/routes/api.js
@@ -17,11 +17,13 @@ router.get('/', (req,res)=>{
     res.send("From api route");
 });
 
+//Defining registration endpoint
 router.post('/register',(req,res)=>{
     //Storing request parameters
     let userData = req.body;
     //creating user object from this data
     let user = new User(userData);
+    
     user.save((err, registeredUser)=>{
         if(err)
             console.log("Registration failed!");
@@ -29,6 +31,29 @@ router.post('/register',(req,res)=>{
             res.status(200).json(registeredUser);
     });
 
+});
+
+//Defining login endpoint
+router.post('/login',(req, res)=>{
+    //Storing request paramters
+    let userData = req.body;
+
+    User.findOne({email : userData.email}, (err, user)=>{
+        if(err)
+            console.log(err);
+        else{
+            if(!user)
+                res.status(401).send("Invalid Email");
+            else if(userData.password !== user.password)
+                res.status(401).send("Invalid Password");
+            else
+                res.status(200).json(user);
+        }
+    });
+});
+// Defining secured expense manager
+router.get('/expenses', (req,res)=>{
+    res.send('Expenses align up here');
 });
 
 export default router;
