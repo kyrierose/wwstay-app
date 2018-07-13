28a2a2fc5b49c064468f2ba0313214e9155c91fb
Akshit Dhar <akshitdhar@live.com>
2018-07-13 21:13:14 +0530

    Installed mongoose

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
+//Main imports
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const PORT = 3000;
+//File Imports
+import api from './routes/api';
 
+const PORT = 3000;
 const app = express();
 
+//responses from api route
+app.use('/api',api);
 app.use(bodyParser.json());
 
+//Handles landing page
 app.get('/',(req, res)=>{
-    res.send("Hello world");
+    res.send("Welcome to wwStay");
+});
+
+//Handles unknown path requests
+app.get('**', (req, res)=>{
+    res.send("Error 404");
 });
 
 app.listen(3000,()=>{
